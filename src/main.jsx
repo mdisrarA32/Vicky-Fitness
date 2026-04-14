50f04835433b6f485f6cfc89613a31c84637599a
Md Israr <mdalam0734@gmail.com>
2026-04-14 23:41:00 +0530

    google analytics connect

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.jsx'
 
+// ✅ Google Analytics Code START
+const script1 = document.createElement("script");
+script1.async = true;
+script1.src = "https://www.googletagmanager.com/gtag/js?id=G-GF2V3VYMM9";
+document.head.appendChild(script1);
+
+const script2 = document.createElement("script");
+script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-GF2V3VYMM9');
+`;
+document.head.appendChild(script2);
+// ✅ Google Analytics Code END
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
