8afa9697d26b0fe6f506d94c5984f3b7a26e94e9
Md Israr <mdalam0734@gmail.com>
2026-04-15 01:21:07 +0530

    feat: initialize React root and mount App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,28 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.jsx'
 
-// ✅ Google Analytics Code START
-const script1 = document.createElement("script");
-script1.async = true;
-script1.src = "https://www.googletagmanager.com/gtag/js?id=G-GF2V3VYMM9";
-document.head.appendChild(script1);
-
-const script2 = document.createElement("script");
-script2.innerHTML = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  window.gtag = gtag;
-
-  gtag('js', new Date());
-
-  gtag('config', 'G-GF2V3VYMM9', {
-    debug_mode: true,
-    send_page_view: true
-  });
-`;
-document.head.appendChild(script2);
-// ✅ Google Analytics Code END
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
